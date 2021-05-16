274d0acbcee408b19518de4b6d8154026b5fe354
abdeddaimmansar <61209218+abdeddaimmansar@users.noreply.github.com>
2021-05-16 18:29:13 +0100

    Update Adherent.php

--- Adherent.php
+++ Adherent.php
@@ -10,8 +10,10 @@
         public $email;
         public $nbr_emprunt;
 
+        public function __construct(){  }
+        public function  __newEntry($id,$nom,$pr,$img,$dep,$tele,$email,$nbr){
 
-        public function  __construct($id,$nom,$pr,$img,$dep,$tele,$email,$nbr){
+
             $this->id_Adh=$id;
             $this->nom_Adh=$nom;
 	        	$this->prenom=$pr;
@@ -28,35 +30,75 @@
 		        $con->ajouterAdherent($this->id_Adh,$this->nom_Adh,$this->prenom,$this->image,$this->depar,$this->tele,$this->email,$this->nbr_emprunt);
         }
 
+        function toUpdate()
+        {
+
+          include_once("conn.php");
+
+          $con = new conn();
+
+          $con->updateAdherent($this->id_Adh,$this->nom_Adh,$this->prenom,$this->image,$this->depar,$this->tele,$this->email,$this->nbr_emprunt);
+
+
+        }
+
     }
-    class Etudiant extends Adherent
+   class Etudiant extends Adherent
 {
   public $cne;
   public $filiere;
   public $annee;
 
-  function __construct($id_Adh,$nom,$prenom,$dest,$depar,$tele,$email,$nbr_emprunt,$cne,$filiere,$annee)
-  {
-     parent:: __construct($id_Adh,$nom,$prenom,$dest,$depar,$tele,$email,$nbr_emprunt);
+   public function __construct(){
+     parent:: __construct();
 
+
+   }
+
+ public  function newEntry($id_Adh,$nom,$prenom,$dest,$depar,$tele,$email,$nbr_emprunt,$cne,$filiere,$annee)
+  {
+     parent:: __newEntry($id_Adh,$nom,$prenom,$dest,$depar,$tele,$email,$nbr_emprunt);
+        echo "child";
        $this->cne=$cne;
        $this->filiere=$filiere;
        $this->annee = $annee;
   }
 public  function addtodata()
-  {  
+  {
      include_once("conn.php");
      $connn = new conn();
       parent:: addtodata();
      $connn->ajouterEtudiant($this->cne,$this->filiere,$this->annee,$this->id_Adh);
+  }
+
+  function waytostudent($id_Adh)
+  {
+
+    include_once("conn.php");
+    $connn = new conn();
+    return $connn->GetEtudiant($id_Adh);
+
+
+
+
+  }
+  function toUpdate()
+  {
+      include("conn.php");
+      parent:: toUpdate();
+
+
+        $conn = new conn();
+
+    $conn->updateStudent($this->cne,$this->filiere,$this->annee,$this->id_Adh);
 
 
   }
 }
     class Enseignant extends Adherent{
 
-        public function  __construct($id_Adh,$nom,$pr,$img,$dep,$tele,$email,$nbr){
-            parent::__construct($id_Adh,$nom,$pr,$img,$dep,$tele,$email,$nbr);
+        public function  newEntry($id_Adh,$nom,$pr,$img,$dep,$tele,$email,$nbr){
+            parent:: newEntry($id_Adh,$nom,$pr,$img,$dep,$tele,$email,$nbr);
 
        }
 
