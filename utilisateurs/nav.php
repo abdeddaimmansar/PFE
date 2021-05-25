59702aedd144e829945ba44c6238a95855f29a5c
Benhamida <78648192+Yousra403@users.noreply.github.com>
2021-05-25 11:43:31 +0100

    Add files via upload

---
+++ utilisateurs/nav.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Dashboard</title>
+
+		<!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/eca-logo.png">
+
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap">
+
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+
+			<!-- Header -->
+            <div class="header">
+
+				<!-- Logo -->
+                <div class="header-left">
+                    <a class="logo">
+						<img src="assets/img/eca-logo.png" alt="Logo">
+					</a>
+                    
+					
+                </div>
+				<!-- /Logo -->
+                
+				<a href="javascript:void(0);" id="toggle_btn">
+					<i class="fas fa-align-left"></i>
+				</a>
+
+				<!-- Search Bar -->
+				<div class="top-nav-search">
+					<form>
+						<input type="text" class="form-control" placeholder="Search here">
+						<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+					</form>
+				</div>
+				<!-- /Search Bar -->
+
+				<!-- Mobile Menu Toggle -->
+				<a class="mobile_btn" id="mobile_btn">
+					<i class="fas fa-bars"></i>
+				</a>
+				<!-- /Mobile Menu Toggle -->
+
+				<!-- Header Right Menu -->
+				<ul class="nav user-menu">
+
+					<!-- Notifications -->
+					<li class="nav-item dropdown noti-dropdown">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+							<i class="far fa-bell"></i> <span class="badge badge-pill"></span>
+						</a>
+						<div class="dropdown-menu notifications">
+							<div class="topnav-dropdown-header">
+								<span class="notification-title">Notifications</span>
+								<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+							</div>
+							<div class="noti-content">
+								<ul class="notification-list">
+									<li class="notification-message">
+										<a href="#">
+											<div class="media">
+
+
+											</div>
+										</a>
+									</li>
+
+
+								</ul>
+							</div>
+							<div class="topnav-dropdown-footer">
+								<a href="#">View all Notifications</a>
+							</div>
+						</div>
+					</li>
+					<!-- /Notifications -->
+
+					<!-- User Menu -->
+					<li class="nav-item dropdown has-arrow">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+							<span class="user-img"><img class="rounded-circle" src="" width="31" alt="Ryan Taylor"></span>
+						</a>
+						<div class="dropdown-menu">
+							<div class="user-header">
+								<div class="avatar avatar-sm">
+									<img src="" alt="User Image" class="avatar-img rounded-circle">
+								</div>
+								<div class="user-text">
+									<h6></h6>
+									<p class="text-muted mb-0"></p>
+								</div>
+							</div>
+							<a class="dropdown-item" href="profile.php">My Profile</a>
+							<a class="dropdown-item" href="login.php">Logout</a>
+						</div>
+					</li>
+					<!-- /User Menu -->
+
+				</ul>
+				<!-- /Header Right Menu -->
+
+            </div>
+			<!-- /Header -->
+
+			<!-- Sidebar -->
+      <div class="sidebar" id="sidebar">
+          <div class="sidebar-inner slimscroll">
+    <div id="sidebar-menu" class="sidebar-menu">
+      <ul>
+        <li class="menu-title">
+          <span>Main Menu</span>
+        </li>
+        <li class="active">
+          <a href="acceuil.php"><i class="fas fa-th-large"></i> <span>Acceuil</span></a>
+        </li>
+        <li class="active">
+          <a href="reglement.php"><i class="far fa-file-alt"></i> <span>Reglement</span> </a>
+         
+        </li>
+        <li class="active">
+          <a href="Listecategories.php"><i class="fas fa-book-reader"></i> <span>Catégories</span> </a>
+         
+        </li>
+      </ul>
+    </div>
+          </div>
+      </div>
+ 
+
+    </body>
+</html>
