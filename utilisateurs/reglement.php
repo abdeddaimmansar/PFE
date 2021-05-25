1a0a922525a0544901f31c9ecc86373ad6069870
Benhamida <78648192+Yousra403@users.noreply.github.com>
2021-05-25 11:42:21 +0100

    Add files via upload

---
+++ utilisateurs/reglement.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/reg.css"/>
+
+
+
+<title>Reglement</title>
+</head>
+
+
+<body>
+<header>
+
+<?php
+        include_once 'nav.php';
+?>
+
+
+
+<div class="row">
+<div class="row align-items-center">
+	<div class="col-sm-12">
+    <h1>Règles de prêt</h1>
+    <div id="reglement_ecrire">
+
+  <h2> Pour Enseignants:</h2>
+      <ul>
+          <li>-La bibliothèque est constituée d'ouvrages déstinés aux prêt et d'autres pour la consultation sur place.</li>
+          <li>-Le nombre d'ouvrages autorisé pour le prêt est limité à quatre.</li>
+          <li>-La durée maximale de prêt est de dix jours.</li>
+          <li>-L'accès à la salle de lecture pour consultation sur place,peut se faire même en dehors des horaires 
+   administratifs,suivant les besoins et les disponibilités.</li>
+      </ul>
+
+  <h2>Pour Etudiants:</h2>
+      <ul>
+          <li>-La bibliothèque est ouverte aux étudiants de l'Ecole sur présentation de la carte de la bibliothèque.</li>
+          <li>-La bibliothèque est constituée d'ouvrages déstinés aux prêt et d'autres pour la consultation sur place.</li>
+          <li>-Le nombre d'ouvrages autorisé pour le prêt est limité à 01.</li>
+          <li>-La durée maximale de prêt est de 05 jours.</li>
+          <li>-L'accès à la salle de lecture pour consultation sur place,peut se faire même en dehors des horaires 
+   administratifs,suivant les besoins et les disponibilités.</li>
+          <li>-Les ouvrages doivent faire l'objet de bons afin de les maintenir en bon état. Toute dégradation sera sanctionnée
+   par la réparation ou même l'achat de l'ouvrage détérioré.</li>
+          <li>-L'accès aux rayonnages est formellement interdit.</li>
+          <li>-Les étudiants sont informés de la liste des ouvrages disponibles ainsi que des nouvelles acquisitions par 
+   voie d'affichage et par listing.</li>
+      </ul>
+  
+</div>
+</div>
+</div>
+
+    
+</div>
+</div>
+	<!-- /Footer -->
+
+</div>
+<!-- /Page Wrapper -->
+
+
+
+</div>
+<!-- /Main Wrapper -->
+
+<!-- jQuery -->
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+
+<!-- Bootstrap Core JS -->
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Slimscroll JS -->
+<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Chart JS -->
+<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+<script src="assets/plugins/apexchart/chart-data.js"></script>
+
+<!-- Custom JS -->
+<script  src="assets/js/script.js"></script>
+</body>
+</html>
