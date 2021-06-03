8c918338add382ddca409dd2fbc1c81be1cbb179
khaoula1611 <80786049+khaoula1611@users.noreply.github.com>
2021-06-03 22:06:25 +0100

    Update ListeLivres.php

--- utilisateurs/ListeLivres.php
+++ utilisateurs/ListeLivres.php
@@ -1,29 +1,23 @@
 <style>
 h2 { color: #0a4870; font-weight: 500;}
 .cards ul { display: flex; flex-wrap: wrap; list-style: none; padding: 0; width:30%;height:25%;}
-ul .booking-card { position: relative; width: 300px;height:400px; display: flex; flex: 0 0 300px; flex-direction: column; margin: 20px; margin-bottom: 30px; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; overflow: hidden;  background-size: cover; text-align: center; color: #0a4870; transition: .3s;}
+ul .booking-card { position: relative; width: 300px;height:400px; display: flex; flex: 0 0 300px; flex-direction: column;border: groove; margin: 20px; margin-bottom: 30px; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; overflow: hidden;  background-size: cover; text-align: center; color: #0a4870; transition: .3s;}
 ul .booking-card::before { content: ''; position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: rgba(10, 72, 112, 0); transition: .3s;}
 ul .booking-card .book-container { height: 200px;}
 ul .booking-card .book-container .content { position: relative; opacity: 0; display: flex; align-items: center; justify-content: center; height: 100%; width: 100%; transform: translateY(-200px); transition: .3s;}
 ul .booking-card .book-container .content .btn { border: 3px solid white; padding: 10px 15px; background: none; text-transform: uppercase; font-weight: bold; font-size: 1.3em; color: white; cursor: pointer; transition: .3s;}
 ul .booking-card .book-container .content .btn:hover { background: white; border: 0px solid white; color: #0a4870;}
-ul .booking-card .informations-container { flex: 1 0 auto; padding: 20px; background: #f0f0f0; transform: translateY(206px); transition: .3s;}
-ul .booking-card .informations-container .title { position: relative; padding-bottom: 10px; margin-bottom: 10px; font-weight: bold; font-size: 1.2em;}
-ul .booking-card .informations-container .title::after { content: ''; position: absolute; bottom: 0; left: 0; right: 0; height: 3px; width: 50px; margin: auto; background: #0a4870;}
-ul .booking-card .informations-container .more-information { opacity: 0; transition: .3s;}
-ul .booking-card .informations-container .more-information .info-and-date-container { display: flex;}
-ul .booking-card .informations-container .more-information .info-and-date-container .box { flex: 1 0; padding: 15px; margin-top: 20px; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; background: white; font-weight: bold; font-size: 0.9em;}
-ul .booking-card .informations-container .more-information .info-and-date-container .box .icon { margin-bottom: 5px;}
-ul .booking-card .informations-container .more-information .disclaimer { margin-top: 20px; font-size: 0.8em; color: #7d7d7d;}
+ul .booking-card .informations-container { flex: 0.3 0.4 auto; height:25px; padding: 2px; background: rgba(76, 67, 67, 0.41);  transform: translateY(20px); transition: .3s;}
+ul .booking-card .informations-container .title { position: relative; padding-bottom: 10px;color:black; margin-bottom: 10px; font-weight: bold; }
+ul .booking-card .informations-container .title::after { content: ''; position: absolute; bottom: 0; left: 0; right: 0; height: 3px; width: 50px; margin: auto; background: black;}
+
 ul .booking-card:hover::before { background: rgba(10, 72, 112, 0.6);}
 ul .booking-card:hover .book-container .content { opacity: 1; transform: translateY(0px);}
-ul .booking-card:hover .informations-container { transform: translateY(0px);}
-ul .booking-card:hover .informations-container .more-information { opacity: 1}
+ul .booking-card:hover .informations-container { transform: translateY(150px);}
 @media (max-width: 768px) {
   ul .booking-card::before {  background: rgba(10, 72, 112, 0.6); }
   ul .booking-card .book-container .content {  opacity: 1;  transform: translateY(0px);}
   ul .booking-card .informations-container { transform: translateY(0px);}
-  ul .booking-card .informations-container .more-information { opacity: 1;}
 }
 </style>
 
@@ -33,18 +27,13 @@ include_once 'nav.php';
 ?>
 
 <?php
- if(isset($_GET['categorie']))
- {
-   $cat=$_GET['categorie'];
+    
    include('conn.php');
    $con=new conn();
-   $result = $con->listeLivres($cat);
+   $result = $con->listeLivres();
    
 
- }
-   else {
-      echo "error";
-   }
+ 
 
 ?>
 
@@ -65,7 +54,8 @@ include_once 'nav.php';
     <!-- /Page Header -->
     <div class="row cards d-flex justify-content-around ">
                 <?php
-                        foreach ($result as $output ) {?>
+                        foreach ($result as $output ) {
+                            ?>
                     <ul  >
                         <li class="booking-card" style="background-image: url(/Admin/<?php echo $output["image"];?>);">
                             <div class="book-container">
@@ -76,23 +66,12 @@ include_once 'nav.php';
                             </div>
                             <div class="informations-container">
                                 <h2 class="title"><?php echo $output["STATUS"];?> </h2>
-                                <div class="more-information">
-                                    <div class="info-and-date-container">
-                                        <div class="box date">
-                                        <svg class="icon" style="width:24px;height:24px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-                                            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 
-                                            2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707
-                                            .455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-                                        </svg>
-                                            <p>Samedi 1er février 2020</p>
-                                        </div>
-                                    </div>
-                                </div>
+                               
                             </div>
                         </li>
                     </ul>
                 <?php } 
-                            
+                         
                             ?>
             </div> 
 
