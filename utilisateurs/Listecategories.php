9f5855daf07c0bd466e5d590f1d2c1b0732c7d84
khaoula1611 <80786049+khaoula1611@users.noreply.github.com>
2021-06-10 16:34:09 +0100

    Update Listecategories.php

--- utilisateurs/Listecategories.php
+++ utilisateurs/Listecategories.php
@@ -28,17 +28,18 @@ ul .booking-card:hover .informations-container .more-information { opacity: 1}
 </style>
 <link rel="stylesheet" href="assets/css/style1.css">
 <?php
+     $bdd = new PDO('mysql:host=127.0.0.1;dbname=biblio;charset=utf8','root','');
     include 'conn.php';
     $cat = new conn();
     $result  = $cat->listeCategorie();
-    $count = $cat->nbrLivres()
+     
     
 ?> 
-
+ 
 <?php
 include_once 'nav.php';
 ?>
-
+<?php echo $result["titre"];?>
     <!-- Page Wrapper -->
     <div class="page-wrapper" style="background-color: rgb(230, 230, 230);">
 
@@ -54,26 +55,26 @@ include_once 'nav.php';
 								</ul>
 							</div>
 							<!-- Search Bar -->
-                            <div class="top-nav-search">
+                            <!-- <div class="top-nav-search">
                                 <form id="chercher" name="chercher" method="POST" action="search.php">
                                     <input type="text" class="form-control" name="contenu" placeholder="Search here" style="background-color: white;">
                                     <button class="btn" name="search" type="submit"><i class="fas fa-search"></i></button>
                                 </form>
-                            </div>
+                            </div> -->
                             <!-- /Search Bar -->
 						</div>
 					</div>
             <!-- /Page Header -->
-
+           
             <div class="row cards d-flex justify-content-around ">
                 <?php
               
                         foreach ($result as $output ) {?>
                     <ul  >
-                        <li class="booking-card" style="background-image: url(/Admin/<?php echo $output["image"];?>);">
+                        <li class="booking-card" style="background-image: url(/Admin/<?php echo $output["image"];?>); background-position: center;">
                             <div class="book-container">
                                 <div class="content">
-                                    <a href="ListeLivres.php?categorie=<?php echo $output["liblecat"];?>">
+                                    <a href="listeLivre_Polycope.php?categorie=<?php echo $output["liblecat"];?>">
                                     <button class="btn">Voir tout</button></a>
                                 </div>
                             </div>
@@ -85,7 +86,7 @@ include_once 'nav.php';
                                         <svg class="icon" style="width:24px;height:24px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
                                             <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
                                         </svg>
-                                            <p>Il y a <?php echo $count;?> Livres</p>
+                                            <p>Il y a <?php $cate= $output["liblecat"]; $count1 = $cat->nbrLivres($cate); echo  '<span style="color:red;">'.$count1.'</span>' ;?> Livres</p>
                                         </div>
                                     </div>
                                 </div>
@@ -97,7 +98,7 @@ include_once 'nav.php';
                                         <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
                                         <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>   
                                     </svg>
-                                       <p>Il y a <?php ?> Polycopes</p>
+                                       <p>Il y a <?php $cate= $output["liblecat"]; $count2 = $cat->nbrPolycopes($cate); echo '<span style="color:red;">'.$count2.'</span>' ;?>  Polycopes</p>
                                         </div>
                                     </div>
                                 </div>
